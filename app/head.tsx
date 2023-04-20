55db3f295edbc6f1d978f2b74e888b913d464e58
EinfachKonrad <EinfachKonrad@gmail.com>
2023-04-20 17:04:19 +0200

    new DocumentHead

---
+++ app/head.tsx
@@ -0,0 +1,31 @@
+export default function Head() {
+
+
+    return (
+      <>
+        <title>Binary Visualizer by EinfachKonrad</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <link rel="icon" href="/favicon.ico" />
+  
+            
+        <meta property="og:locale" content="de" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Binary Visualizer by EinfachKonrad" />
+  
+        <meta name="description" content="Offizielle Website der Schülerzeitung des evangelischen Schulzentrums Leipzig seit 2021!" />
+        <meta property="og:description" content="Offizielle Website der Schülerzeitung des evangelischen Schulzentrums Leipzig seit 2021!" />
+  
+        <meta property="og:url" content="http://binary.nexcessing-it.com" />
+        <meta property="og:secure_url" content="https://binary.nexcessing-it.com" />
+  
+        <meta property="og:image" content="https://binary.nexcessing-it.com/favicon.ico"/>
+        <meta property="og:image:alt" content="logo" />
+  
+        <meta content="#ffffff" data-react-helmet="true" name="theme-color" />
+  
+        <meta name="copyright" content="EinfachMedia Webmedienproduktion und Eventtechnik Leipzig" />	
+  
+      </>
+    );
+  }
+  
