eea5e12de73e811932854656d4fe7f86e412dafd
EinfachKonrad <EinfachKonrad@gmail.com>
2023-04-25 23:17:04 +0200

    just some stuff for typescript, and some other stuff

--- app/page.tsx
+++ app/page.tsx
@@ -6,28 +6,6 @@ import React, { useState } from 'react';
 function Home() {
   const [numbers, setNumbers] = useState([]);
 
-  // Definiere einen State, der ein Objekt mit den Schlüsseln 1 bis 8 und den Werten 0 oder 1 enthält
-  const [binaryCode, setBinaryCode] = useState({
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
-    6: 0,
-    7: 0,
-    8: 0,
-  });
-
-  // Ändere die handleNumber-Funktion, so dass sie neben dem Wert von number auch die id der Bit-Komponente als Argument nimmt
-  function handleNumber(number, id) {
-    setNumbers((numbers) => [...numbers, number]);
-
-    // Aktualisiere den State, indem du den Wert von number dem entsprechenden Schlüssel in dem Objekt zuweist
-    setBinaryCode((binaryCode) => ({
-      ...binaryCode,
-      [id]: number,
-    }));
-  }
 
   return (
     <>
@@ -37,20 +15,19 @@ function Home() {
       </div>
 
       <div className="grid grid-cols-8 gap-4 my-[5%] mx-[10%]">
-        {/* Übergib die Funktion als Prop an jede Bit-Komponente */}
-        <Bit id="8" onNumber={handleNumber} />
-        <Bit id="7" onNumber={handleNumber} />
-        <Bit id="6" onNumber={handleNumber} />
-        <Bit id="5" onNumber={handleNumber} />
-        <Bit id="4" onNumber={handleNumber} />
-        <Bit id="3" onNumber={handleNumber} />
-        <Bit id="2" onNumber={handleNumber} />
-        <Bit id="1" onNumber={handleNumber} />
+        <Bit id="8" />
+        <Bit id="7" />
+        <Bit id="6" />
+        <Bit id="5" />
+        <Bit id="4" />
+        <Bit id="3" />
+        <Bit id="2" />
+        <Bit id="1" />
       </div>
 
       {/* Rende ein Element, das den Binärcode anzeigt */}
       <div className="text-center text-4xl font-bold">
-        Binärcode: {Object.values(binaryCode).join('')}
+        <h2 id='decimal'>0</h2>
       </div>
     </>
   );
