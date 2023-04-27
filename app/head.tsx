a2897a2d958c9b9ddda2bda9e023c76395921462
Konrad <72760187+EinfachKonrad@users.noreply.github.com>
2023-04-27 08:18:57 +0200

    fixed the meta:desc in the head

--- app/head.tsx
+++ app/head.tsx
@@ -13,8 +13,8 @@ export default function Head() {
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Binary Visualizer by EinfachKonrad" />
   
-        <meta name="description" content="A simple visualizer to understand how the binary system works!" />
-        <meta property="og:description" content="A simple visualizer to understand how the binary system works!" />
+        <meta name="description" content="A simple visualizer to understand how the binary numeric system works!" />
+        <meta property="og:description" content="A simple visualizer to understand how the binary numeric system works!" />
   
         <meta property="og:url" content="http://binary.nexcessing-it.com" />
         <meta property="og:secure_url" content="https://binary.nexcessing-it.com" />
